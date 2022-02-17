f6f3cac4488a35625804ca584ca63f6bace1e7ca
arakawahokuto <4869bigdipper@gmail.com>
2022-02-17 19:38:24 +0900

    2枚目

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// ------------jQuery----------------
+
+
+// ------------JavaScript-------------
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------jQuery------------------------------------------------------
 $(function(){
 
+
+    // -------ヘッダーのハンバーガーメニュー--------
     $(".toggle_btn").on("click",function(){
         if($("#header").hasClass("open")){
             $("#header").removeClass("open");
@@ -16,14 +31,20 @@ $(function(){
     $("#navi a").on("click",function(){
         $("header").removeClass("open");
     });
+// --------------------------------------------
 
+$("#openModal").on("click",function(){
+    $("#openModal").fadeIn();
+    $(".slick-slider").slick("setPosition");
+});
 
+$("#closeModal, #modalBig").on("click",function(){
+    $("#modalArea").fadeOut();
+});
 
-
-
-
-
-
+$(".single_sample").slick({
+    dots: true,
+});
 
 
 
