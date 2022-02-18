a6ca0fe90ee923aedc42e71b14b5357c4be84784
arakawahokuto <4869bigdipper@gmail.com>
2022-02-18 18:02:19 +0900

    コミットドキドキ

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 
 // ------------JavaScript-------------
 
-const swiper = new Swiper(".swiper", { 
-    pagination: {
-      el: ".swiper-pagination"
-    },
-    navigation: {   
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
+
+// // ----------商品画像のスライド----------------
+// const swiper = new Swiper(".swiper", { 
+//     pagination: {
+//       el: ".swiper-pagination"
+//     },
+//     navigation: {   
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev"
+//     }
+//   });
+// // -----------------------------------------
+
+
 
   
 
@@ -37,10 +42,47 @@ $(function(){
     });
 // --------------------------------------------
     
-    
 
 
+// --------商品画像のモーダル-----------
 
+    // $('.image').modaal({
+    //     type: 'image'
+    // });
+
+// -----------------------------------
+
+
+    // let item = $(".item");
+    // let wh = $(window).height();
+    // let now = $(window).scrollTop() + wh;
+    // let position =$(".inner-title").offset().top;
+
+    let item = $(".item");
+    // let now = $(window).scrollTop();
+
+  $(window).on("scroll",function() {
+    let now = $(window).scrollTop();
+    if (now <= 300 || now > 2500) {
+        item.removeClass("on")
+        console.log(now);
+      } 
+    else if (now > 300) {
+      item.addClass("on")
+      console.log(now);
+    } 
+  });
+  
+
+//     $(window).scroll(function() {
+//     if (now > position) {
+//       $(".item").addClass("hide");
+
+//     } else if (now < position) {
+//     //   $(".item").removeClass("hide");
+//     $(".item").removeClass("hide");
+//     }
+//   });
 
 
 
