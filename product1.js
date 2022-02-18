1206b6b67d90c88f5f385bb629f9e30945e2c902
arakawahokuto <4869bigdipper@gmail.com>
2022-02-18 20:38:45 +0900

    コミットドキドキなんです

--- product1.js
+++ product1.js
@@ -15,6 +15,23 @@ const swiper = new Swiper(".swiper", {
 
 $(function(){
 
+    $(".toggle_btn").on("click",function(){
+        if($("#header").hasClass("open")){
+            $("#header").removeClass("open");
+        }else{
+            $("#header").addClass("open");
+        }
+    });
+
+    $("#mask").on("click",function(){
+        $("#header").removeClass("open");
+    });
+
+    $("#navi a").on("click",function(){
+        $("header").removeClass("open");
+    });
+
+
     $('.image').modaal({
         type: 'image'
     });
